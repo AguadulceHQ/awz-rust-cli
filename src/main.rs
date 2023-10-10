2400250a03ebcb7cc70c0c7021eb7c217680ce1b
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-10-10 21:51:57 +0700

    feat: customized cli to be closer to the macro calculator use case

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,31 @@ use std::process::exit;
 
 fn main() {
     let mut cli_prompt = CliPrompt::new();
-    cli_prompt.intro("example app").unwrap();
+    cli_prompt.intro("This is a basic CLI tool").unwrap();
 
-    cli_prompt.prompt_text("Enter your name").unwrap();
+    let name = cli_prompt.prompt_text("What is your name?").unwrap();
 
-    let answer = cli_prompt.prompt_confirm("Are you sure?").unwrap();
+    cli_prompt
+        .log(&format!("Your name is {}", &name), LogType::Info)
+        .unwrap();
+
+    let age: u8 = cli_prompt
+        .prompt_text("What is your age?")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    cli_prompt
+        .log(&format!("You are {} years old", &age), LogType::Info)
+        .unwrap();
+
+    cli_prompt
+        .log(&format!("Adulthood: {}", is_adult(age)), LogType::Info)
+        .unwrap();
+
+    let answer = cli_prompt
+        .prompt_confirm("Is the information filled correct?")
+        .unwrap();
 
     if !answer {
         cli_prompt.cancel("Operation cancelled").unwrap();
@@ -15,16 +35,29 @@ fn main() {
     }
 
     let options = vec![
-        PromptSelectOption::new("option1", "Pikachu"),
-        PromptSelectOption::new("option2", "Charmander"),
-        PromptSelectOption::new("option3", "Squirtle"),
+        PromptSelectOption::new("option1", "Sedentary"),
+        PromptSelectOption::new("option2", "Lightly Active"),
+        PromptSelectOption::new("option3", "Moderately Active"),
+        PromptSelectOption::new("option4", "Super Active"),
+        PromptSelectOption::new("option5", "Very Active"),
     ];
     let selected_option = cli_prompt
-        .prompt_select("Which one do you prefer?", options)
+        .prompt_select("How active are you?", options)
         .unwrap();
 
     cli_prompt
         .log(&format!("{}", selected_option), LogType::Info)
         .unwrap();
-    cli_prompt.outro("Good Bye").unwrap();
+    cli_prompt
+        .outro("Thanks for using our application. Bye 👋")
+        .unwrap();
+}
+
+fn is_adult(age: u8) -> bool {
+    const THRESHOLD_FOR_MAJORITY: u8 = 18;
+
+    if age >= THRESHOLD_FOR_MAJORITY {
+        return true;
+    }
+    return false;
 }
